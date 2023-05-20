3896026f650e8bc48a511c8d8eaf9ae4023fa4dd
novalabsug <novalabsug@gmail.com>
2023-05-20 23:15:02 +0300

    This code is for windows

--- src/App/Main.js
+++ src/App/Main.js
@@ -1,182 +1,177 @@
 import React, { useState } from "react";
-import '../App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Deposit from '../images/Path 80.png';
-import Networth from '../images/Path 3.png';
-import './style.scss';
-import Modal from 'react-bootstrap/Modal';
-import Learn from '../Accounts/Learn';
+import "../App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Deposit from "../images/Path 80.png";
+import Networth from "../images/Path 3.png";
+import "./style.scss";
+import Modal from "react-bootstrap/Modal";
+import Learn from "../Accounts/Learn";
 import axios from "axios";
 import { API_URL_GET_AUTH_USER, TOKEN } from "../api";
 
 const Main = ({ id, activeTab, children, ...props }) => {
-    const [show1, setShow1] = useState(false);
-    const handleClose1 = () => setShow1(false);
-    const handleShow1 = () => setShow1(true);
-    const [deposit, setDeposit] = useState("")
-    const [networth, setNetworth] = useState("")
-    const [dollarDeposit, setDollarDeposit] = useState("")
-    const [dollarNetworth, setDollarNetworth] = useState("")
-    axios.get(`${API_URL_GET_AUTH_USER}`, {
-            headers: {
-                "Authorization": `Token ${TOKEN}`
-            }
-        }).then((res) => {
-            if (res.data.currency === "UGX") {
-                setDeposit(res.data.deposit_amount)
-                setNetworth(res.data.net_worth)
-            } else {
-                setDollarDeposit(res.data.deposit_amount)
-                setDollarNetworth(res.data.net_worth)
-            }
-        })
-        .catch((error) => {
-            console.error(error)
-        })
-    return ( <
-        div className = " p-lg-2 rounded-25 dollar scroll-y" >
-        <
-        div className = " rounded-25 d-none d-lg-block" >
-
-        <
-        h6 > YOUR STATISTICS < /h6>   <
-        div className = "row" >
-        <
-        div className = "col-lg-9 col-md-12" > <
-        div className = "d-flex flex-row flex py-2 " >
-        <
-        div className = "p-lg-4 peach rounded-25 w-50 text-center" >
-        <
-        h5 className = "bolder mt-4" > Deposit < /h5> <
-        div className = "d-flex flex-row flex justify-content-center" > UGX <
-        h1 className = "px-2 font-lighter" > { deposit } < /h1></div >
-        <
-        img src = { Deposit }
-        className = "pt-2 d-none d-sm-block"
-        width = '100%'
-        height = '30%'
-        alt = "investors" / >
-        <
-        /
-        div >
-        <
-        div className = "blue-dark p-lg-4 mx-2 rounded-25 w-50 text-center" >
-        <
-        h5 className = "bolder mt-4" > Networth < /h5> <
-        div className = "d-flex flex-row flex justify-content-center" > UGX <
-        h1 className = "px-2 font-lighter" > { networth } < /h1></div >
-        <
-        img src = { Networth }
-        className = "pt-2 d-none d-sm-block"
-        width = '100%'
-        height = '20%'
-        alt = "investors" / >
-        <
-        /
-        div >
-        <
-        /div>  <
-        div >
-        <
-        div className = "py-5" >
-        <
-        h6 > DOLLAR ACCOUNT < /h6>     <
-        div className = "d-flex flex-row flex mt-3 my-2 " >
-        <
-        div className = "p-lg-5 light shadow rounded-25 w-50 text-center" > <
-        h5 className = "bolder mt-4" > Deposit < /h5>  <
-        div className = "d-flex flex-row flex justify-content-center" > USD <
-        h1 className = "px-2 font-lighter" > 0.0 < /h1></div > < /
-        div >
-        <
-        div className = "shadow light rounded-25 p-lg-5 mx-3 px-lg-3 w-50 text-center" >
-        <
-        h5 className = "bolder mt-4" > Networth < /h5> <
-        div className = "d-flex flex-row flex justify-content-center" > USD <
-        h1 className = "px-2 font-lighter" > 0.0 < /h1></div >
-        <
-        /
-        div > <
-        /div>  < /
-        div > <
-        div className = "row rounded-25 w-100" >
-        <
-        div className = "col-lg-8 px-lg-5 mt-3" > <
-        h4 className = "bolder" > Welcome to Cyanase < /h4>  <
-        h6 > Investments products, loans, sacco groups, investment clubs all in one package.Including our API
-        for integration < /h6> <h6>What products are you looking for? < /
-        h6 > < /
-        div > <
-        div className = "col-lg-4 p-lg-5 text-center" >
-        <
-        h6 className = "px-lg-5 py-3 rounded-4 border border-warning text-warning text-center"
-        onClick = { handleShow1 } > Learn More < /h6> < /
-        div >
-        <
-        Modal show = { show1 }
-        onHide = { handleClose1 }
-        dialogClassName = "my-modal1" >
-        <
-        Learn close = { handleClose1 }
-        tab2 = { props.handletab2 }
-        / > < /
-        Modal > < /
-        div >
-        <
-        /
-        div >
-        <
-        /
-        div > <
-        div className = "col-lg-3 d-none d-sm-block rounded-25 blue-dark py-5 px-2 text-center" >
-        <
-        h5 className = "bolder" > Your Statistics < /h5>  <
-        h5 className = "pt-5 bolder" > Activity < /h5>   <
-        div className = "d-flex flex-row flex justify-content-center" >
-        <
-        div className = "w-25" > 0 % < /div> <
-        div className = "w-25" > 0 % < /div> <
-        div className = "w-25" > 0 % < /div> < /
-        div >
-        <
-        div className = "d-flex flex-row flex justify-content-center" >
-        <
-        div className = "w-25" > Deposits < /div> <
-        div className = "w-25" > Loans < /div> <
-        div className = "w-25" > Withdraws < /div> < /
-        div >
-        <
-        img src = { Networth }
-        className = "py-2 mt-3"
-        width = '80%'
-        height = '10%'
-        alt = "investors" / >
-        <
-        h6 className = "pt-5 bolder" > Total Deposits < /h6> <
-        div className = "d-flex flex-row flex justify-content-center" > UGX <
-        h3 className = "px-2 font-lighter" > 0.0 < /h3></div >
-        <
-        img src = { Networth }
-        className = "py-2 mt-3"
-        width = '80%'
-        height = '10%'
-        alt = "investors" / >
-        <
-        h6 className = "pt-5 bolder" > Total Networth < /h6>  <
-        div className = "d-flex flex-row flex justify-content-center" > UGX <
-        h3 className = "px-2 font-lighter" > 0.0 < /h3></div >
-        <
-        img src = { Networth }
-        className = "py-2 mt-3"
-        width = '80%'
-        height = '10%'
-        alt = "investors" / >
-        <
-        /
-        div > < /
-        div > < /
-        div > < /div>
-    );
+  const [show1, setShow1] = useState(false);
+  const handleClose1 = () => setShow1(false);
+  const handleShow1 = () => setShow1(true);
+  const [deposit, setDeposit] = useState("");
+  const [networth, setNetworth] = useState("");
+  const [dollarDeposit, setDollarDeposit] = useState("");
+  const [dollarNetworth, setDollarNetworth] = useState("");
+  axios
+    .get(`${API_URL_GET_AUTH_USER}`, {
+      headers: {
+        Authorization: `Token ${TOKEN}`,
+      },
+    })
+    .then((res) => {
+      if (res.data.currency === "UGX") {
+        setDeposit(res.data.deposit_amount);
+        setNetworth(res.data.net_worth);
+      } else {
+        setDollarDeposit(res.data.deposit_amount);
+        setDollarNetworth(res.data.net_worth);
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  return (
+    <div className=" p-lg-2 rounded-4 dollar scroll-y">
+      <div className=" rounded-4 d-none d-lg-block">
+        <h6> YOUR STATISTICS </h6>{" "}
+        <div className="row">
+          <div className="col-lg-9 col-md-12">
+            {" "}
+            <div className="d-flex flex-row flex py-2 ">
+              <div className="p-lg-3 peach rounded-4 w-50 text-center">
+                <h5 className="bolder mt-4 mt-lg-3"> Deposit </h5>{" "}
+                <div className="d-flex flex-row flex justify-content-center">
+                  {" "}
+                  UGX <h1 className="px-2 font-lighter"> {deposit} </h1>
+                </div>
+                <img
+                  src={Deposit}
+                  className="pt-2 d-none d-sm-block"
+                  width="100%"
+                  height="30%"
+                  alt="investors"
+                />
+              </div>
+              <div className="blue-dark p-lg-3 mx-2 rounded-25 w-50 text-center">
+                <h5 className="bolder mt-4 mt-3"> Networth </h5>{" "}
+                <div className="d-flex flex-row flex justify-content-center">
+                  {" "}
+                  UGX <h1 className="px-2 font-lighter"> {networth} </h1>
+                </div>
+                <img
+                  src={Networth}
+                  className="pt-2 d-none d-sm-block"
+                  width="100%"
+                  height="20%"
+                  alt="investors"
+                />
+              </div>
+            </div>{" "}
+            <div>
+              <div className="py-5 py-lg-3">
+                <h6> DOLLAR ACCOUNT </h6>{" "}
+                <div className="d-flex flex-row flex mt-3 my-2 ">
+                  <div className="p-lg-4 light shadow rounded-4 w-50 text-center">
+                    {" "}
+                    <h5 className="bolder mt-4 mt-lg-3"> Deposit </h5>{" "}
+                    <div className="d-flex flex-row flex justify-content-center">
+                      {" "}
+                      USD <h1 className="px-2 font-lighter"> 0.0 </h1>
+                    </div>{" "}
+                  </div>
+                  <div className="shadow light rounded-4 p-lg-4 mx-3 px-lg-3 w-50 text-center">
+                    <h5 className="bolder mt-4 mt-lg-3"> Networth </h5>{" "}
+                    <div className="d-flex flex-row flex justify-content-center">
+                      {" "}
+                      USD <h1 className="px-2 font-lighter"> 0.0 </h1>
+                    </div>
+                  </div>{" "}
+                </div>{" "}
+              </div>{" "}
+              <div className="row rounded-4 w-100">
+                <div className="col-lg-8 px-lg-4 mt-3">
+                  {" "}
+                  <h4 className="bolder"> Welcome to Cyanase </h4>{" "}
+                  <h6>
+                    {" "}
+                    Investments products, loans, sacco groups, investment clubs
+                    all in one package.Including our API for integration{" "}
+                  </h6>{" "}
+                  <h6>What products are you looking for? </h6>{" "}
+                </div>{" "}
+                <div className="col-lg-4 p-lg-4 text-center">
+                  <h6
+                    className="px-lg-4 py-3 rounded-4 border border-warning text-warning text-center"
+                    onClick={handleShow1}
+                  >
+                    {" "}
+                    Learn More{" "}
+                  </h6>{" "}
+                </div>
+                <Modal
+                  show={show1}
+                  onHide={handleClose1}
+                  dialogClassName="my-modal1"
+                >
+                  <Learn close={handleClose1} tab2={props.handletab2} />{" "}
+                </Modal>{" "}
+              </div>
+            </div>
+          </div>{" "}
+          <div className="col-lg-3 d-none d-sm-block rounded-4 blue-dark py-5 py-lg-4 px-2 text-center">
+            <h5 className="bolder"> Your Statistics </h5>{" "}
+            <h5 className="pt-5 pt-lg-3 bolder"> Activity </h5>{" "}
+            <div className="d-flex flex-row flex justify-content-center">
+              <div className="w-25"> 0 % </div>{" "}
+              <div className="w-25"> 0 % </div>{" "}
+              <div className="w-25"> 0 % </div>{" "}
+            </div>
+            <div className="d-flex flex-row flex justify-content-center">
+              <div className="w-25"> Deposits </div>{" "}
+              <div className="w-25"> Loans </div>{" "}
+              <div className="w-25"> Withdraws </div>{" "}
+            </div>
+            <img
+              src={Networth}
+              className="py-2 mt-3"
+              width="80%"
+              height="10%"
+              alt="investors"
+            />
+            <h6 className="pt-5 pt-lg-3 bolder"> Total Deposits </h6>{" "}
+            <div className="d-flex flex-row flex justify-content-center">
+              {" "}
+              UGX <h3 className="px-2 font-lighter"> 0.0 </h3>
+            </div>
+            <img
+              src={Networth}
+              className="py-2 mt-3"
+              width="80%"
+              height="10%"
+              alt="investors"
+            />
+            <h6 className="pt-5 pt-lg-3 bolder"> Total Networth </h6>{" "}
+            <div className="d-flex flex-row flex justify-content-center">
+              {" "}
+              UGX <h3 className="px-2 font-lighter"> 0.0 </h3>
+            </div>
+            <img
+              src={Networth}
+              className="py-2 mt-3"
+              width="80%"
+              height="10%"
+              alt="investors"
+            />
+          </div>{" "}
+        </div>{" "}
+      </div>{" "}
+    </div>
+  );
 };
 
 export default Main;
--- src/App/Personal.js
+++ src/App/Personal.js
@@ -1,176 +1,187 @@
 import React, { useState } from "react";
-import '../App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Wallet from '../images/wallet.png';
+import "../App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Wallet from "../images/wallet.png";
 import ProgressBar from "@ramonak/react-progress-bar";
-import './style.scss';
-import Offcanvas from 'react-bootstrap/Offcanvas';
-import TopUp from '../Accounts/TopUp';
-import Goal1 from '../Accounts/Goal1';
-import Modal from 'react-bootstrap/Modal';
-import Learn1 from '../Accounts/Learn1';
+import "./style.scss";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import TopUp from "../Accounts/TopUp";
+import Goal1 from "../Accounts/Goal1";
+import Modal from "react-bootstrap/Modal";
+import Learn1 from "../Accounts/Learn1";
 import { AddUser } from "react-iconly";
 
 const Personal = ({ id, activeTab, children, name, ...props }) => {
-    const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-    const [show1, setShow1] = useState(false);
-    const handleClose1 = () => setShow1(false);
-    const handleShow1 = () => setShow1(true);
-    const [show2, setShow2] = useState(false);
+  const [show1, setShow1] = useState(false);
+  const handleClose1 = () => setShow1(false);
+  const handleShow1 = () => setShow1(true);
+  const [show2, setShow2] = useState(false);
 
-    const handleClose2 = () => setShow2(false);
-    const handleShow2 = () => setShow2(true);
-    console.log(props)
+  const handleClose2 = () => setShow2(false);
+  const handleShow2 = () => setShow2(true);
+  console.log(props);
 
-    return ( <
-        div > <
-        div className = "row scroll-y " > <
-        div className = "col-8 bg-light p-3 rounded-25 " >
-        <
-        h6 className = " p-2 mt-2" > WALLET < /h6>  <
-        div className = "row bg-white rounded-25 p-5" >
-        <
-        div className = " px-3 text-center " >
-        <
-        img src = { Wallet }
-        className = "pt-2"
-        width = '10%'
-        height = '30%'
-        alt = "investors" / > <
-        h5 className = "bolder mt-5" > Account Balance < /h5> <
-        div className = "d-flex flex-row flex justify-content-center w-100" > UGX <
-        h1 className = "px-2 font-lighter" > 0.0 < /h1> < /
-        div >
-        <
-        div className = "d-flex flex-row flex justify-content-center" >
-        <
-        h6 className = "px-5 py-3 mt-3 border border-warning text-warning rounded-4"
-        onClick = { handleShow } >
-        Top Up < /h6>  <
-        Offcanvas show = { show }
-        placement = "end"
-        className = "side-bar"
-        onHide = { handleClose } {...props } >
-        <
-        img src = { Wallet }
-        className = "pt-2 text-center"
-        width = '100'
-        height = '100'
-        alt = "investors" / >
-        <
-        Offcanvas.Header
-        // closeButton
-        >
-        <
-        div className = "row" > <
-        Offcanvas.Title className = "bolder" > Top Up < /Offcanvas.Title> </div >
-        <
-        /
-        Offcanvas.Header > <
-        Offcanvas.Body className = "px-5" >
-        <
-        TopUp / > < /
-        Offcanvas.Body > < /
-        Offcanvas > <
-        h6 className = "px-5 py-3 mt-3 mx-2 border d-none border-warning text-warning rounded-25"
-        onClick = { handleShow2 } >
-        Deposit < /h6> </div >
-        <
-        Modal show = { show2 }
-        onHide = { handleClose2 }
-        dialogClassName = "my-modal1" >
-        <
-        Learn1 tab9 = { props.handletab9 }
-        / > < /
-        Modal > < /
-        div >
-        <
-        /div>  <
-        div >
-
-        <
-        h6 className = "pt-5" > RECENT ACTIVITY < /h6>   <
-        div className = "row mt-3 px-4 bg-white rounded" >
-        <
-        div className = "col-3" >
-        <
-        p className = "pt-3" > < span className = "bolder" > Deposit < /span> <
-        p > UGX 10, 000 < /p>   < /
-        p >
-        <
-        /
-        div >
-        <
-        div className = "col-6" >
-        <
-        h6 className = "px-5 pt-4 active text-center" > Successful < /h6> < /
-        div >
-        <
-        div className = "col-3 text-end" >
-        <
-        p className = "pt-3" > < span className = "bolder" > 21 Jan < /span> <
-        p > 3: 30 EAT <
-        /p>   < /
-        p > <
-        /div > < /
-        div >
-        <
-        /
-        div >
-        <
-        /
-        div > <
-        div className = "col-4 rounded-25 px-4" > <
-        div className = "row p-2 bg-light rounded-3" >
-        <
-        div className = "text-start col-6 p-2" > YOUR PERSONAL GOALS < /div> <
-        div className = "text-end col-6 p-2" > < span className = " px-2 py-1 bolder" > 1 < /span> < /div > < /
-        div >
-        <
-        div className = "p-4 bg-white shadow-sm rounded-25 mt-3" >
-        <
-        div className = "d-flex flex-row flex" >
-        <
-        span className = "mt-2" > <
-        AddUser className = " rounded-circle border border-dark p-1"
-        size = "large" / > < /span> <
-        p className = "mx-4 mt-2" > < span className = "bolder" > Build a Mansion < /span> ...Created: 3 Aug</p >
-        <
-        /div> <
-        h6 className = "mt-5" > Progress: 3 months to go < /h6>  <
-        ProgressBar completed = { 80 }
-        customLabel = ""
-        isLabelVisible = { false }
-        completedClassName = "barCompleted"
-        maxCompletedClassName = "barMaxCompleted"
-        maxCompleted = { 200 }
-        barContainerClassName = "container" /
-        >
-        <
-        p className = "mt-5" > Total Deposit < /p> <
-        p className = "text-center bg-light bolder p-2 rounded-3" > < span className = " mx-2" > UGX < /span> 10,000 < /p >
-        <
-        /
-        div >
-
-        <
-        h6 className = "px-5 py-3 mt-3 border border-warning text-center text-warning rounded-4"
-        onClick = { handleShow1 } >
-        New Goal < /h6>  <
-        Offcanvas show = { show1 }
-        placement = "end"
-        className = "side-barsy pt-5"
-        onHide = { handleClose1 } {...props } > <
-        Goal1 close1 = { handleClose1 }
-        / > < /
-        Offcanvas > < /
-        div > < /
-        div > < /
-        div >
-    );
+  return (
+    <div>
+      {" "}
+      <div className="row scroll-y ">
+        {" "}
+        <div className="col-8 bg-light p-3 rounded-4 ">
+          <h6 className=" p-2 mt-2"> WALLET </h6>{" "}
+          <div className="row bg-white rounded-25 p-5">
+            <div className=" px-3 text-center ">
+              <img
+                src={Wallet}
+                className="pt-2"
+                width="10%"
+                height="30%"
+                alt="investors"
+              />{" "}
+              <h5 className="bolder mt-5 mt-lg-3"> Account Balance </h5>{" "}
+              <div className="d-flex flex-row flex justify-content-center w-100">
+                {" "}
+                UGX <h1 className="px-2 font-lighter"> 0.0 </h1>{" "}
+              </div>
+              <div className="d-flex flex-row flex justify-content-center">
+                <h6
+                  className="px-5 py-3 mt-3 border border-warning text-warning rounded-4"
+                  onClick={handleShow}
+                >
+                  Top Up{" "}
+                </h6>{" "}
+                <Offcanvas
+                  show={show}
+                  placement="end"
+                  className="side-bar"
+                  onHide={handleClose}
+                  {...props}
+                >
+                  <img
+                    src={Wallet}
+                    className="pt-2 text-center"
+                    width="100"
+                    height="100"
+                    alt="investors"
+                  />
+                  <Offcanvas.Header
+                  // closeButton
+                  >
+                    <div className="row">
+                      {" "}
+                      <Offcanvas.Title className="bolder">
+                        {" "}
+                        Top Up{" "}
+                      </Offcanvas.Title>{" "}
+                    </div>
+                  </Offcanvas.Header>{" "}
+                  <Offcanvas.Body className="px-5">
+                    <TopUp />{" "}
+                  </Offcanvas.Body>{" "}
+                </Offcanvas>{" "}
+                <h6
+                  className="px-5 py-3 mt-3 mx-2 border d-none border-warning text-warning rounded-25"
+                  onClick={handleShow2}
+                >
+                  Deposit{" "}
+                </h6>{" "}
+              </div>
+              <Modal
+                show={show2}
+                onHide={handleClose2}
+                dialogClassName="my-modal1"
+              >
+                <Learn1 tab9={props.handletab9} />{" "}
+              </Modal>{" "}
+            </div>
+          </div>{" "}
+          <div>
+            <h6 className="pt-5 pt-lg-4"> RECENT ACTIVITY </h6>{" "}
+            <div className="row mt-3 px-4 bg-white rounded">
+              <div className="col-3">
+                <p className="pt-3">
+                  {" "}
+                  <span className="bolder"> Deposit </span> <p> UGX 10, 000 </p>{" "}
+                </p>
+              </div>
+              <div className="col-6">
+                <h6 className="px-5 pt-4 active text-center"> Successful </h6>{" "}
+              </div>
+              <div className="col-3 text-end">
+                <p className="pt-3">
+                  {" "}
+                  <span className="bolder"> 21 Jan </span> <p> 3: 30 EAT </p>{" "}
+                </p>{" "}
+              </div>{" "}
+            </div>
+          </div>
+        </div>{" "}
+        <div className="col-4 rounded-25 px-4">
+          {" "}
+          <div className="row p-2 bg-light rounded-3">
+            <div className="text-start col-6 p-2">
+              {" "}
+              <h3>YOUR PERSONAL GOALS</h3>{" "}
+            </div>{" "}
+            <div className="text-end col-6 p-2">
+              {" "}
+              <span className=" px-2 py-1 bolder"> 1 </span>{" "}
+            </div>{" "}
+          </div>
+          <div className="p-4 bg-white shadow-sm rounded-25 mt-3">
+            <div className="d-flex flex-row flex">
+              <span className="mt-2">
+                {" "}
+                <AddUser
+                  className=" rounded-circle border border-dark p-1"
+                  size="large"
+                />{" "}
+              </span>{" "}
+              <p className="mx-4 mt-2">
+                {" "}
+                <span className="bolder"> Build a Mansion </span> ...Created: 3
+                Aug
+              </p>
+            </div>{" "}
+            <h6 className="mt-5 mt-lg-3"> Progress: 3 months to go </h6>{" "}
+            <ProgressBar
+              completed={80}
+              customLabel=""
+              isLabelVisible={false}
+              completedClassName="barCompleted"
+              maxCompletedClassName="barMaxCompleted"
+              maxCompleted={200}
+              barContainerClassName="container"
+            />
+            <p className="mt-5 mt-lg-3"> Total Deposit </p>{" "}
+            <p className="text-center bg-light bolder p-2 rounded-3">
+              {" "}
+              <span className=" mx-2"> UGX </span> 10,000{" "}
+            </p>
+          </div>
+          <h6
+            className="px-5 py-3 mt-3 border border-warning text-center text-warning rounded-4"
+            onClick={handleShow1}
+          >
+            New Goal{" "}
+          </h6>{" "}
+          <Offcanvas
+            show={show1}
+            placement="end"
+            className="side-barsy pt-5 mt-lg-3"
+            onHide={handleClose1}
+            {...props}
+          >
+            {" "}
+            <Goal1 close1={handleClose1} />{" "}
+          </Offcanvas>{" "}
+        </div>{" "}
+      </div>{" "}
+    </div>
+  );
 };
 
 export default Personal;
